96b6269fa30f4ca2b1ba5870ef13fd86ce80ac42
AJarOfDirt <bdnorstad@gmail.com>
2024-10-12 11:02:13 -0700

    Add variant option for "fur" types using custom tag "Has_Female_Variants"

--- Source/FemaleBodyVariants/GetFloatMenuPatch.cs
+++ Source/FemaleBodyVariants/GetFloatMenuPatch.cs
@@ -20,7 +20,7 @@ namespace FemaleBodyVariants
                 return;
             }
 
-            string graphicPath = pawn.story.bodyType.bodyNakedGraphicPath;
+           string graphicPath = pawn.story.bodyType.bodyNakedGraphicPath;
             if (pawn.gender != Gender.Male && graphicPath != null && !graphicPath.Contains("_Female") && (graphicPath.Contains("_Thin") || graphicPath.Contains("_Fat") || graphicPath.Contains("_Hulk")))
             {
                 graphicPath += "_Female";
@@ -30,4 +30,25 @@ namespace FemaleBodyVariants
         }
     }
 
+    [HarmonyPatch(typeof(PawnRenderNode_Fur), nameof(PawnRenderNode_Fur.GraphicFor))]
+    public static class PawnRenderNode_Fur_GraphicFor_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(PawnRenderNode_Fur __instance, ref Pawn pawn, ref Graphic __result)
+        {
+            string graphicPath = pawn.story.furDef.GetFurBodyGraphicPath(pawn);
+            if (pawn.gender != Gender.Male && graphicPath != null && !graphicPath.Contains("_Female") && (graphicPath.Contains("_Thin") || graphicPath.Contains("_Fat") || graphicPath.Contains("_Hulk")))
+            {
+                bool hasXenoVariants = pawn.genes.Xenogenes.Any(gene => gene.def.exclusionTags.Any(tag => tag == "Has_Female_Variants") && !gene.Overridden);
+                bool hasEndoVariants = pawn.genes.Endogenes.Any(gene => gene.def.exclusionTags.Any(tag => tag == "Has_Female_Variants") && !gene.Overridden);
+                Log.Message($"[FBV]: haxXenoVariant={hasXenoVariants}, hasEndoVariant={hasEndoVariants}, current graphicPath={graphicPath}");
+                if (hasXenoVariants || hasEndoVariants)
+                {
+                    graphicPath += "_Female";
+                    Shader shader = __instance.ShaderFor(pawn);
+                    __result = GraphicDatabase.Get<Graphic_Multi>(graphicPath, shader, Vector2.one, __instance.ColorFor(pawn));
+                }
+            }
+        }
+    }
 }
